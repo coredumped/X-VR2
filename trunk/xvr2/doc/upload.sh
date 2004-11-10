8f36aaa553075db44fc8636d9114987bde605c83
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2004-11-10 05:20:46 +0000

    Fixed to upload docs to sourceforge
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@7 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/xvr2/doc/upload.sh
+++ trunk/xvr2/doc/upload.sh
@@ -1,1 +1,2 @@
-scp -r html/* root@devel.esolutions4all.com:/opt/xvr2/website/manual-z/
+#scp -r html/* mindstorm2600@shell.sf.net:/opt/xvr2/website/manual-z/
+scp -r html/* mindstorm2600@shell.sf.net:/home/groups/x/xv/xvr2/htdocs/
