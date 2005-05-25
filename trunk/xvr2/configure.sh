361bb0d2b169cab68c930f0d5fd372c21e666b69
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2005-05-25 23:40:19 +0000

    Adding more flexibility
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@104 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/xvr2/configure.sh
+++ trunk/xvr2/configure.sh
@@ -16,32 +16,82 @@ cd db
 ./configure.sh "$@"
 cd $THIS_DIR
 
-cd drivers
+cd math
+./configure.sh "$@"
+cd $THIS_DIR
+
+cd gl
+./configure.sh "$@"
+cd $THIS_DIR
+
+cd ui
 ./configure.sh "$@"
 cd $THIS_DIR
 
 
 
-echo "all: base/src/libxvr2.so.${VERSION} step2 step3 step4
+echo "
+baselib=libxvr2.so.${VERSION}
+utillib=libxvr2util.so.${VERSION}
+dblib=libxvr2db.so.${VERSION}
+netlib=libxvr2net.so.${VERSION}
+mathlib=libxvr2math.so.${VERSION}
+gllib=libxvr2gl.so.${VERSION}
+uilib=libxvr2ui.so.${VERSION}
+
+baselibpath=base/src/${baselib}
+utillibpath=util/src/${utillib}
+dblibpath=db/src/${dblib}
+netlibpath=net/src/${netlib}
+mathlibpath=math/src/${mathlib}
+gllibpath=gl/src/${gllib}
+uilibpath=ui/src/${uilib}
+
+all: step1 step2 step3 step4 step5 step6 step7 installmsg
+
+step1: ${baselibpath}
+
+step2: ${utillibpath}
+
+step3: ${dblibpath}
+
+step4: ${netlibpath}
+
+step5: ${mathlibpath}
+
+step6: ${gllibpath}
+
+step7: ${uilibpath}
 
 docs: step100
 
-base/src/libxvr2.so.${VERSION}: base/src/*.h base/src/*.cpp
+${baselibpath}: base/src/*.h base/src/*.cpp
 	cd base/src ; make -f Makefile
 
-step2: base/src/libxvr2.so.${VERSION} util/src/*.h util/src/*.cpp
+${utillibpath}: step1 util/src/*.h util/src/*.cpp
 	cd util/src ; make -f Makefile
 
-step3: base/src/libxvr2.so.${VERSION} step2 db/src/*.h db/src/*.cpp
+${dblibpath}: step1 step2 db/src/*.h db/src/*.cpp
 	cd db/src ; make -f Makefile
 
-step4: base/src/libxvr2.so.${VERSION} step2 net/src/*.h net/src/*.cpp
+${netlibpath}: step1 step2 net/src/*.h net/src/*.cpp
 	cd net/src ; make -f Makefile
+
+${mathlibpath}: step1 step2 math/src/*.h math/src/*.cpp
+	cd math/src ; make -f Makefile
+
+${gllibpath}: step1 step2 step5 gl/src/*.h gl/src/*.cpp
+	cd gl/src ; make -f Makefile
+
+${uilibpath}: step1 step2 step5 step6 ui/src/*.h ui/src/*.cpp
+	cd ui/src ; make -f Makefile
+
+installmsg:
 	@echo
 	@echo
 	@echo 'Now (as root) type: make install'
 
-drivers: base/src/libxvr2.so.${VERSION} all
+drivers: step1 step2 step3 step4
 	cd drivers ; make -f Makefile
 	@echo
 	@echo
@@ -61,6 +111,9 @@ clean:
 	cd util/src ; make -f Makefile clean
 	cd net/src ; make -f Makefile clean
 	cd db/src ; make -f Makefile clean
+	cd math/src ; make -f Makefile clean
+	cd gl/src ; make -f Makefile clean
+	cd ui/src ; make -f Makefile clean
 	cd drivers ; make -f Makefile clean
 
 install:
@@ -68,6 +121,9 @@ install:
 	cd util/src ; make -f Makefile install
 	cd db/src ; make -f Makefile install
 	cd net/src ; make -f Makefile install
+	cd math/src ; make -f Makefile install
+	cd gl/src ; make -f Makefile install
+	cd ui/src ; make -f Makefile install
 	@echo
 	@echo "To compile the provided drivers type:"
 	@echo "make drivers"
