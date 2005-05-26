e7b65c3397d9de103623dafd539b29ce98f62d99
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2005-05-26 01:28:59 +0000

    Adding CVS checking stuff
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@109 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/xvr2/buildtools/functions.sh
+++ trunk/xvr2/buildtools/functions.sh
@@ -8,8 +8,18 @@ STANDARD_BIN_PATHS="/opt/sfw/bin:/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/u
 find_binary()
 {
 	binname=$1
+	echo -ne "Checking ${binname}... " >&2
+	#FIRST SEE IF THE FILE IS ALREADY IN THE CACHE, IF IT IS THEN GRAB IT FROM THERE
+	if [ -f .build_tools.fscache ]; then
+		BINX=`grep "^${binname}:" .build_tools.fscache`
+		if [ $? -eq 0 ]; then
+			echo -ne "cached " >&2
+			echo $BINX | cut -f2 -d:
+			return 0
+		fi
+	fi
 	gotsomething=0
-	echo -ne "Checking ${binname}..." > 2
+
 	for dir in `echo $STANDARD_BIN_PATHS | tr -s ':' ' '`
 	do
 		for file in echo $dir/*
@@ -18,13 +28,14 @@ find_binary()
 			if [ "$ffile" = "$binname" ]; then
 				if [ -x $dir/$binname ]; then
 					echo $dir/$binname
+					echo "$binname:$dir/$binname" >> .build_tools.fscache
 					gotsomething=1
 				fi
 			fi
 		done
 	done
 	if [ $gotsomething -eq 0 ]; then
-		echo "$binname NOT FOUND" 1>&2
+		echo "NOT FOUND" >&2
 		return 1
 	fi
 	return 0
@@ -50,3 +61,20 @@ find_flex()
 	find_binary flex
 }
 
+get_gcc_version()
+{
+	bin=$1
+	$bin -v 2>&1 | grep ^gcc | awk '{print $3}' | cut -f1 -d.
+}
+
+get_gcc_revision()
+{
+	bin=$1
+	$bin -v 2>&1 | grep ^gcc | awk '{print $3}' | cut -f2- -d. | sed 's/\./_/g'
+}
+
+get_gcc_full_version()
+{
+	bin=$1
+	$bin -v 2>&1 | grep ^gcc | awk '{print $3}' | sed 's/\./_/g'
+}
