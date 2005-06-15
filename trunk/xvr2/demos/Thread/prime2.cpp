9741033881573ba8aebd5d7d75010dd3eb26cc9a
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2005-06-15 00:48:13 +0000

    Adding test cases
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@141 07303567-df1b-0410-b539-9fd000affbe4

---
+++ trunk/xvr2/demos/Thread/prime2.cpp
@@ -0,0 +1,77 @@
+#include<xvr2.h>
+
+using namespace xvr2;
+
+Mutex om;
+
+SharedVar<int> tnum;
+
+class Prime:public Thread {
+	private:
+		int f, t;
+	public:
+		Prime(int _f, int _t){
+			f = _f;
+			t = _t;
+		}
+		void joinTo(Prime *to);
+		void *run();
+};
+
+void Prime::joinTo(Prime *to){
+	to->join();
+}
+
+void *Prime::run(){
+	int i, j;
+	bool flag;
+	String output;
+	tnum++;
+	output = "Prime numbers from ";
+	output += f;
+	output += " to ";
+	output += t;
+	output += ":\n";
+	for(i = f; i < t; i++){
+		flag = true;
+		for(j = 2; j < i; j++){
+			if(i % j == 0){
+				flag = false;
+				break;
+			}
+		}
+		if(flag){
+			output += i;
+			output += " ";
+		}
+	}
+	om.lock();
+	std::cout << output << std::endl;
+	om.unlock();
+	tnum--;
+	return 0;
+}
+
+int main(){
+	Prime *a;
+	Prime *b;
+	Prime *c;
+	tnum.setValue(0);
+
+	a = new Prime(10000, 120000);
+	b = new Prime(20000, 220000);
+	c = new Prime(60000, 610000);
+
+	a->start();
+	b->start();
+	c->start();
+	b->joinTo(c);
+	sleep(5);
+	while(tnum.getValue() > 0){
+		sleep(1);
+	}
+	delete a;
+	delete b;
+	delete c;
+	return 0;
+}
