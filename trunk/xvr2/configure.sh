69c422fcd56491e5fb305611ac1b8918a21a92ff
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2005-06-20 20:28:54 +0000

    Fixing meta-include directory
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@156 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/xvr2/configure.sh
+++ trunk/xvr2/configure.sh
@@ -1,43 +1,55 @@
 VERSION=`cat VERSION`
 THIS_DIR=`pwd`
+baselib=libxvr2.so
+utillib=libxvr2util.so
+dblib=libxvr2db.so
+netlib=libxvr2net.so
+mathlib=libxvr2math.so
+gllib=libxvr2gl.so
+uilib=libxvr2ui.so
+
+mkdir -p include/xvr2
+
 cd base
-./configure.sh "$@"
+./configure.sh "$@" --version="$VERSION" --with-name=$baselib
 cd $THIS_DIR
 
 cd util
-./configure.sh "$@"
+./configure.sh "$@" --version="$VERSION" --with-name=$utillib
 cd $THIS_DIR
 
 cd net
-./configure.sh "$@"
+./configure.sh "$@" --version="$VERSION" --with-name=$netlib
 cd $THIS_DIR
 
 cd db
-./configure.sh "$@"
+./configure.sh "$@" --version="$VERSION" --with-name=$dblib
 cd $THIS_DIR
 
 cd math
-./configure.sh "$@"
+./configure.sh "$@" --version="$VERSION" --with-name=$mathlib
 cd $THIS_DIR
 
 cd gl
-./configure.sh "$@"
+./configure.sh "$@" --version="$VERSION" --with-name=$gllib
 cd $THIS_DIR
 
 cd ui
-./configure.sh "$@"
+./configure.sh "$@" --version="$VERSION" --with-name=$uilib
 cd $THIS_DIR
 
-
+cd drivers
+./configure.sh "$@" --version="$VERSION"
+cd $THIS_DIR
 
 echo "
-baselib=libxvr2.so.${VERSION}
-utillib=libxvr2util.so.${VERSION}
-dblib=libxvr2db.so.${VERSION}
-netlib=libxvr2net.so.${VERSION}
-mathlib=libxvr2math.so.${VERSION}
-gllib=libxvr2gl.so.${VERSION}
-uilib=libxvr2ui.so.${VERSION}
+baselib=${baselib}.${VERSION}
+utillib=${utillib}.${VERSION}
+dblib=${dblib}.${VERSION}
+netlib=${netlib}.${VERSION}
+mathlib=${mathlib}.${VERSION}
+gllib=${gllib}.${VERSION}
+uilib=${uilib}.${VERSION}
 
 baselibpath=base/src/${baselib}
 utillibpath=util/src/${utillib}
@@ -49,42 +61,52 @@ uilibpath=ui/src/${uilib}
 
 all: step1 step2 step3 step4 step5 step6 step7 installmsg
 
-step1: ${baselibpath}
+step0: include/xvr2
+	mkdir -p include/xvr2
+	cp -f base/src/*.h include/xvr2
+	cp -f db/src/*.h include/xvr2
+	cp -f util/src/*.h include/xvr2
+	cp -f net/src/*.h include/xvr2
+	cp -f math/src/*.h include/xvr2
+	cp -f gl/src/*.h include/xvr2
+	cp -f ui/src/*.h include/xvr2
 
-step2: ${utillibpath}
+step1: step0 \$(baselibpath)
 
-step3: ${dblibpath}
+step2: \$(utillibpath)
 
-step4: ${netlibpath}
+step3: \$(dblibpath)
 
-step5: ${mathlibpath}
+step4: \$(netlibpath)
 
-step6: ${gllibpath}
+step5: \$(mathlibpath)
 
-step7: ${uilibpath}
+step6: \$(gllibpath)
+
+step7: \$(uilibpath)
 
 docs: step100
 
-${baselibpath}: base/src/*.h base/src/*.cpp
-	cd base/src ; make -f Makefile
+\$(baselibpath): base/src/*.h base/src/*.cpp
+	cd base/src ; cp -f *.h ../../include/xvr2/ ; make -f Makefile
 
-${utillibpath}: step1 util/src/*.h util/src/*.cpp
-	cd util/src ; make -f Makefile
+\$(utillibpath): step1 util/src/*.h util/src/*.cpp
+	cd util/src ; cp -f *.h ../../include/xvr2/ ; make -f Makefile
 
-${dblibpath}: step1 step2 db/src/*.h db/src/*.cpp
-	cd db/src ; make -f Makefile
+\$(dblibpath): step1 step2 db/src/*.h db/src/*.cpp
+	cd db/src ; cp -f *.h ../../include/xvr2/ ; make -f Makefile
 
-${netlibpath}: step1 step2 net/src/*.h net/src/*.cpp
-	cd net/src ; make -f Makefile
+\$(netlibpath): step1 step2 net/src/*.h net/src/*.cpp
+	cd net/src ; cp -f *.h ../../include/xvr2/ ; make -f Makefile
 
-${mathlibpath}: step1 step2 math/src/*.h math/src/*.cpp
-	cd math/src ; make -f Makefile
+\$(mathlibpath): step1 step2 math/src/*.h math/src/*.cpp
+	cd math/src ; cp -f *.h ../../include/xvr2/ ; make -f Makefile
 
-${gllibpath}: step1 step2 step5 gl/src/*.h gl/src/*.cpp
-	cd gl/src ; make -f Makefile
+\$(gllibpath): step1 step2 step5 gl/src/*.h gl/src/*.cpp
+	cd gl/src ; cp -f *.h ../../include/xvr2/ ; make -f Makefile
 
-${uilibpath}: step1 step2 step5 step6 ui/src/*.h ui/src/*.cpp
-	cd ui/src ; make -f Makefile
+\$(uilibpath): step1 step2 step5 step6 ui/src/*.h ui/src/*.cpp
+	cd ui/src ; cp -f *.h ../../include/xvr2/ ; make -f Makefile
 
 installmsg:
 	@echo
@@ -107,6 +129,7 @@ step100:
 	
 clean:
 	rm -rf doc/html doc/latex
+	@rm -rf include/xvr2/*
 	cd base/src ; make -f Makefile clean
 	cd util/src ; make -f Makefile clean
 	cd net/src ; make -f Makefile clean
