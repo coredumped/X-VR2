0dd8bc0bb218a2dfce9271a21f640c8aaa7494ae
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2005-06-20 20:46:49 +0000

    Fixed
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@162 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/xvr2/drivers/configure.sh
+++ trunk/xvr2/drivers/configure.sh
@@ -2,15 +2,18 @@ THIS_DIR=`pwd`
 VERSION=`cat ../VERSION`
 #DEPS="../../base/src/Plugin.h ../../base/src/System.h ../../base/src/Exception.h "
 ALL=
-DRIVERS=`ls | grep -v CVS`
+DRIVERS=`ls | grep -v CVS | grep -v database | grep -v graphics`
 for dir in `echo $DRIVERS`
 do
 	if [ -d $dir ]; then
-		echo "Configuring $dir driver..."
-		cd $dir
-		./configure.sh "$@" --version=${VERSION}
-		cd ..
-		ALL="${ALL} ${dir}/${dir}-driver.so.${VERSION}"
+		#Only process if it has a configure.sh script
+		if [ -f $dir/configure.sh ]; then
+			echo "Configuring $dir driver..."
+			cd $dir
+			./configure.sh "$@" --version=${VERSION}
+			cd ..
+			ALL="${ALL} ${dir}/${dir}-driver.so.${VERSION}"
+		fi
 	fi
 done
 echo "VERSION=${VERSION}
@@ -24,13 +27,16 @@ all: ${ALL}
 for dir in `echo $DRIVERS`
 do
 	if [ -d $dir ]; then
-		echo "${dir}/${dir}-driver.so.${VERSION}: ${DEPS} ${dir}/*.h ${dir}/*.cpp" >> Makefile
-		echo -e "\tcd ${dir} ; make -f Makefile" >> Makefile
-		echo "" >> Makefile
-		echo "${dir}/*.h:" >> Makefile
-		echo "" >> Makefile
-		echo "${dir}/*.cpp:" >> Makefile
-		echo "" >> Makefile
+		#Only process if it has a Makefile
+		if [ -f $dir/Makefile ]; then
+			echo "${dir}/${dir}-driver.so.${VERSION}: ${DEPS} ${dir}/*.h ${dir}/*.cpp" >> Makefile
+			echo -e "\tcd ${dir} ; make -f Makefile" >> Makefile
+			echo "" >> Makefile
+			echo "${dir}/*.h:" >> Makefile
+			echo "" >> Makefile
+			echo "${dir}/*.cpp:" >> Makefile
+			echo "" >> Makefile
+		fi
 	fi
 done
 
@@ -40,7 +46,10 @@ clean:" >> Makefile
 for dir in `echo $DRIVERS`
 do
 	if [ -d $dir ]; then
-		echo -e "\tcd ${dir} ; make -f Makefile clean" >> Makefile
+		#Only process if it has a Makefile
+		if [ -f $dir/Makefile ]; then
+			echo -e "\tcd ${dir} ; make -f Makefile clean" >> Makefile
+		fi
 	fi
 done
 
@@ -50,7 +59,10 @@ install:" >> Makefile
 for dir in `echo $DRIVERS`
 do
 	if [ -d $dir ]; then
-		echo -e "\tcd ${dir} ; make -f Makefile install" >> Makefile
+		#Only process if it has a Makefile
+		if [ -f $dir/Makefile ]; then
+			echo -e "\tcd ${dir} ; make -f Makefile install" >> Makefile
+		fi
 	fi
 done
 
