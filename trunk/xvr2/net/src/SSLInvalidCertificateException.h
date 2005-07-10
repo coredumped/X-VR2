12ab0c8a5d697419c51f0aa39680fb73ae5c8f11
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2005-07-10 04:48:26 +0000

    Adding missing header
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@189 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/xvr2/net/src/SSLInvalidCertificateException.h
+++ trunk/xvr2/net/src/SSLInvalidCertificateException.h
@@ -5,7 +5,7 @@
 #define __XVR2_NET_SSLInvalidCertificate_EXCEPTION_H__
 #include<xvr2/Exception.h>
 #include<xvr2/IOException.h>
-#include<xvr2/NetworkException.h>
+#include<xvr2/SSLGenericException.h>
 namespace xvr2 {
 	namespace Exception{
 
