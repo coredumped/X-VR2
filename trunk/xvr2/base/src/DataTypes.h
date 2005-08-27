b543ded78761d5e43d36442defe7d4b9dc6de56e
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2005-08-27 16:51:45 +0000

    Now using the STDINT defined types if available
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@230 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/xvr2/base/src/DataTypes.h
+++ trunk/xvr2/base/src/DataTypes.h
@@ -7,21 +7,24 @@
 #define __DATA_TYPES_H__
 
 #include<xvr2config.h>
-
+#ifdef _STDINT_H
+typedef uint8_t			UInt8;
+typedef int8_t			Int8;
+typedef uint16_t	  	UInt16;
+typedef int16_t			Int16;
+typedef uint32_t		UInt32;
+typedef int32_t			Int32;
+typedef uint64_t		UInt64;
+typedef int64_t			Int64;
+#else
 typedef unsigned char		UInt8;
 typedef signed char		Int8;
 typedef unsigned short  	UInt16;
 typedef signed short    	Int16;
 typedef unsigned int    	UInt32;
 typedef signed int      	Int32;
-
 typedef unsigned long long	UInt64;
 typedef long long		Int64;
-/*typedef struct {
-        UInt32 hi;
-        UInt32 lo;
-} UInt64, Int64;*/
-
-typedef int		TCPsocket;
+#endif
 
 #endif
