93ca72d371ba1580c71fa5459deb72f1c907fea3
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2005-09-06 19:57:47 +0000

    Fixing debug messages
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@240 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/xvr2/base/src/Mutex.cpp
+++ trunk/xvr2/base/src/Mutex.cpp
@@ -14,6 +14,12 @@
 #include<iostream>
 #include<errno.h>
 
+#ifdef DEBUG_MUTEXES
+using std::cout;
+using std::cerr;
+using std::endl;
+#endif
+
 namespace xvr2{
 
 Mutex::Mutex(int autoinit){
@@ -53,43 +59,49 @@ void Mutex::init(){
 
 void Mutex::lock(){
 #ifdef USE_GNUPTH
-	#ifdef DEBUG_MUTEXES
+	/*#ifdef DEBUG_MUTEXES
 		cout << "Mutex::lock " << (unsigned int)&mutex << " in " << (unsigned int)pth_self() << endl;
-	#endif
+	#endif*/
 	pth_mutex_acquire(&mutex);
 #else
 
 	#ifdef USE_SDL
 	//////////////// SDL CODE STARTS HERE //////////////
-		#ifdef DEBUG_MUTEXES
+		/*#ifdef DEBUG_MUTEXES
 			cout << "Mutex::lock " << (unsigned int)&mutex << " in " << (unsigned int)SDL_ThreadID() << endl;
-		#endif
+		#endif*/
 		if(SDL_mutexP(mutex) == -1)
 			throw Exception::UnableToLockMutex();
 	//////////////// SDL CODE ENDS HERE //////////////
 	#else
  
  
-		#ifdef DEBUG_MUTEXES
-			cout << "Mutex::lock " << (unsigned int)&mutex << " in " << (unsigned int)pthread_self() << endl;
-		#endif
 		int err_code;
 		if((err_code = pthread_mutex_lock(&mutex)) != 0){
-			#ifdef USING_LINUX
+#ifdef USING_LINUX
 				if(err_code == EDEADLK){
-					#ifdef DEBUG_MUTEXES
-						cerr << "Mutex: " << (unsigned int)&mutex << " in " << (unsigned int)SDL_ThreadID() <<  " will create a DEADLOCK " << endl;
-					#endif
+#ifdef DEBUG_MUTEXES
+					//cerr << "Mutex: " << (unsigned int)&mutex << " in " << (unsigned int)SDL_ThreadID() <<  " will create a DEADLOCK " << endl;
+					debugmsgln(this, "DEADLOCK detected");
+#endif
 					throw Exception::DeadLock();
 				}
-				else
-			#endif
+				else if(err_code == EINVAL){
+					debugmsgln(this, " MUTEX HAS NOT BEEN INITIALIZED CORRECTLY!!!!");
 					throw Exception::UnableToLockMutex();
+				}
+#else
+				throw Exception::UnableToLockMutex();
+#endif
 		}
 	#endif
 
 
 
+#endif
+#ifdef DEBUG_MUTEXES
+	//cout << "Mutex::lock " << (unsigned int)&mutex << " in " << (unsigned int)pthread_self() << endl;
+	debugmsgln(this, "Locked");
 #endif
 }
 
@@ -97,25 +109,28 @@ void Mutex::lock(){
 void Mutex::unlock(){
 #ifdef USE_GNUPTH
 	pth_mutex_release(&mutex);
-#ifdef DEBUG_MUTEXES
+/*#ifdef DEBUG_MUTEXES
  cout << "Mutex::unlock " << (unsigned int)&mutex << " in " << (unsigned int)pth_self() << endl;
-#endif
+#endif*/
 #else
 	#ifdef USE_SDL
 	//////////////// SDL CODE STARTS HERE //////////////
 		if(SDL_mutexV(mutex) == -1)
 			throw Exception::UnableToUnlockMutex();
-		#ifdef DEBUG_MUTEXES
+		/*#ifdef DEBUG_MUTEXES
 			cout << "Mutex::unlock " << (unsigned int)&mutex << " in " << (unsigned int)SDL_ThreadID() << endl;
-		#endif
+		#endif*/
 	//////////////// SDL CODE ENDS HERE //////////////
 	 #else
 		 if(pthread_mutex_unlock(&mutex) != 0)
 			 throw Exception::UnableToUnlockMutex();
-		#ifdef DEBUG_MUTEXES
+		/*#ifdef DEBUG_MUTEXES
 			cout << "Mutex::unlock " << (unsigned int)&mutex << " in " << (unsigned int)pthread_self() << endl;
-		#endif
+		#endif*/
 	#endif
+#endif
+#ifdef DEBUG_MUTEXES
+	debugmsgln(this, "Unlocked");
 #endif
 }
 
