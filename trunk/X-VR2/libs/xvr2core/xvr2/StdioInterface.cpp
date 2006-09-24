20cc0ba1a9029058341c5c0ba42100788e03ebc8
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2006-09-24 15:33:51 +0000

    Adding Stdio Interfacing class
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@380 07303567-df1b-0410-b539-9fd000affbe4

---
+++ trunk/X-VR2/libs/xvr2core/xvr2/StdioInterface.cpp
@@ -0,0 +1,101 @@
+/* $Id$ */
+#include"StdioInterface.h"
+#include"SystemException.h"
+#include<cstdio>
+#include<cerrno>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+
+namespace xvr2 {
+	void StdioInterface::_close(){
+		if(_fptr != 0){
+			if(fclose(_fptr) != 0){
+				throw SystemException(errno);
+			}
+			_fptr = 0;
+		}
+	}
+	
+	void StdioInterface::_open(const String &fname, const String &mode){
+		_fptr = fopen(fname.toCharPtr(), mode.toCharPtr());
+		if(_fptr == 0) throw SystemException(errno);
+	}
+	
+	bool StdioInterface::_opened(){
+		return (_fptr == 0)?false:true;
+	}
+	
+	size_t StdioInterface::_read(void *_data, size_t __bytes){
+		size_t ret;
+#ifdef XVR2_STDIO_PARANOID
+		if(!_opened()) throw FileNotOpened();
+#endif
+		ret = fread(_data, 1, __bytes, _fptr);
+		if(ferror(_fptr)){
+			throw StdioInterfaceException(errno);
+		}
+		return ret;
+	}
+	
+	size_t StdioInterface::_write(const void *_data, size_t __bytes){
+		size_t ret;
+#ifdef XVR2_STDIO_PARANOID
+		if(!_opened()) throw FileNotOpened();
+#endif
+		ret = fwrite(_data, 1, __bytes, _fptr);
+		if(ferror(_fptr)){
+			throw StdioInterfaceException(errno);
+		}
+		return ret;
+	}
+	
+	size_t StdioInterface::computeSize(){
+		if(_fsize == 0){
+#ifdef XVR2_STDIO_PARANOID
+			if(!_opened()) throw FileNotOpened();
+#endif
+			struct stat inf;
+			if(fstat(fileno(_fptr), &inf) != 0){
+				throw SystemException(errno);
+			}
+			_fsize = inf.st_size;	
+		}		
+		return _fsize;
+	}
+	
+	void StdioInterface::_seek(UInt32 _pos, UInt32 _from){
+#ifdef XVR2_STDIO_PARANOID	
+		if(!_opened()) throw FileNotOpened();
+#endif
+		if(fseek(_fptr, _pos, _from) != 0) throw SystemException(errno);
+	}
+	
+	void StdioInterface::_seek(UInt32 _pos){
+		_seek(_pos, SEEK_SET);
+	}
+	
+	void StdioInterface::_seekBegin(){
+		_seek(0, SEEK_SET);
+	}
+	
+	void StdioInterface::_seekEnd(){
+		_seek(0, SEEK_END);
+	}
+	
+	void StdioInterface::_seekStep(UInt32 step){
+		_seek(step, SEEK_CUR);
+	}
+	
+	UInt32 StdioInterface::_tell(){
+		long _tx;
+#ifdef XVR2_STDIO_PARANOID
+		if(!_opened()) throw FileNotOpened();
+#endif
+		if((_tx = ftell(_fptr)) == -1) throw SystemException(errno);
+		return (UInt32)_tx;
+	}
+	
+	StdioInterfaceException::StdioInterfaceException(int _code) : SystemException(_code){	}
+	
+};
---
+++ trunk/X-VR2/libs/xvr2core/xvr2/StdioInterface.h
@@ -0,0 +1,46 @@
+#ifndef __XVR2_STDIOINTERFACE_H__
+#define __XVR2_STDIOINTERFACE_H__
+#include<xvr2/Object.h>
+#include<xvr2/String.h>
+#include<xvr2/SystemException.h>
+#include<cstdio>
+
+namespace xvr2 {
+	/** TODO: Document this class */
+	class StdioInterface : public Object {
+		private:
+		protected:
+			FILE *_fptr;
+			void _close();
+			void _open(const String &fname, const String &mode);
+			bool _opened();
+			size_t _read(void *_data, size_t __bytes);
+			size_t _write(const void *_data, size_t __bytes);
+			size_t computeSize();
+			void _seek(UInt32 _pos, UInt32 _from);
+			void _seek(UInt32 _pos);
+			void _seekBegin();
+			void _seekEnd();
+			void _seekStep(UInt32 step);
+			UInt32 _tell();
+			size_t _fsize;
+		public:
+			StdioInterface(){
+				_fptr = 0;
+				_fsize = 0;
+			}
+			FILE *cFILE() const {
+				return _fptr;
+			}
+	};
+	
+	/** TODO: Document this class */
+	class StdioInterfaceException : public SystemException {
+		private:
+		protected:
+		public:
+			StdioInterfaceException(int _code);
+	};
+};
+
+#endif
