11ffac3d0aff58d21c9c8dfd9251c2c9dd51edc6
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2006-10-25 23:37:16 +0000

    Basing Log on StdioOutputFile
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@433 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/X-VR2/libs/xvr2core/xvr2/Log.cpp
+++ trunk/X-VR2/libs/xvr2core/xvr2/Log.cpp
@@ -3,8 +3,24 @@
  *
  * $Id$
  */
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 #include"config.h"
 #include"Log.h"
+#include<xvr2/Timestamp.h>
 
 namespace xvr2{
 
@@ -19,34 +35,32 @@ namespace xvr2{
 
 	//Start implementation of class: Log
 
-	Log::Log(const String &fname){
+	Log::Log(const String &fname, bool _show_times):StdioOutputFile(fname){
 #if __GNUC__ < 3
 		setClassName(xvr2::_xvr2Log);
 #endif
-		f = new BaseFile(fname);
+		show_times = _show_times;
 	}
 
-	Log::~Log(){
-		xvr2_delete(f);
+	static const char *__lsep = ": ";
+
+	Log &Log::write(void *data, UInt32 size){
+		if(NL.equals((char *)data)){
+			StdioOutputFile::write((void *)NL.toCharPtr(), NL.size());
+			flush();
+			return *this;
+		}
+		if(show_times){
+			Timestamp t;
+			std::string s = t.toString();
+			StdioOutputFile::write((void *)s.c_str(), s.size());
+			StdioOutputFile::write((void *)__lsep, 2);
+		}
+		StdioOutputFile::write((void *)data, size);
+		return *this;
 	}
 
-	void Log::log(const char *s){
-		f->open(BaseFile::APPEND);
-		f->write(s, strlen(s));
-		f->close();
-	}
-
-	void Log::log(const String &s){
-		f->open(BaseFile::APPEND);
-		//f->write(s->toCharPtr(), s.size());
-		f->close();
-	}
-
-	void Log::logf(const char *fmt, ...){
-	}
-
-	//void Log::(){
-	//}
 
 	//End implementation of class: Log
 };
+
--- trunk/X-VR2/libs/xvr2core/xvr2/Log.h
+++ trunk/X-VR2/libs/xvr2core/xvr2/Log.h
@@ -1,37 +1,41 @@
 /*
  * $Id$
  */
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 #ifndef __XVR2_Log_H__
 #define __XVR2_Log_H__
 
-#include<xvr2/System.h>
-#include<xvr2/BaseFile.h>
 #include<xvr2/Mutex.h>
 #include<xvr2/CoreExceptions.h>
+#include<xvr2/StdioOutputFile.h>
 
 namespace xvr2{
-	/**
-	 * This class manages basic log file handling
-	 *
-	 * STATUS: IN PROGRESS
-	 *
-	 */
-	class Log:public System{
+	/** This class manages basic log file handling */
+	class Log:public StdioOutputFile {
 		private:
 			Mutex mutex;
-			BaseFile *f;
 		protected:
-			void open();
-			void close();
+			bool show_times;
 		public:
-			Log(const String &fname);
-			~Log();
-			void log(const char *s);
-			void log(const String &s);
-			void logf(const char *fmt, ...);
-			void logf(const String &s, ...);
+			Log(const String &fname, bool _show_times = true);
+			Log &write(void *data, UInt32 size);
 			
 	};
 };
 
 #endif
+
