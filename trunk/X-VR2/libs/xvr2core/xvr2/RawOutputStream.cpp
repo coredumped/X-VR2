ead52a33f264c0ccb9568256d7d0586e89174b0d
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2006-10-30 18:31:02 +0000

    Adding the RawOutputStream class
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@443 07303567-df1b-0410-b539-9fd000affbe4

---
+++ trunk/X-VR2/libs/xvr2core/xvr2/RawOutputStream.cpp
@@ -0,0 +1,103 @@
+/* $Id$ */
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#include"RawOutputStream.h"
+#include<cerrno>
+#include<sys/types.h>
+#include<sys/poll.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+namespace xvr2 {
+	RawOutputStream::RawOutputStream(){
+		//As a default initialize it to be the stdout
+		_fd = 1;
+	}
+
+	RawOutputStream::~RawOutputStream(){
+		close();
+	}
+
+	RawOutputStream::RawOutputStream(int __fd){
+		open(__fd);
+	}
+
+	RawOutputStream::RawOutputStream(const String &fname){
+		open(fname);
+	}
+
+	void RawOutputStream::open(int __fd){
+		_fd = __fd;
+		_a_close = false;
+	}
+
+	void RawOutputStream::open(const String &fname){
+		_fd = ::open(fname.toCharPtr(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if(_fd == -1) throw SystemException(errno);
+		_a_close = true;
+	}
+
+	void RawOutputStream::close(){
+		if(_fd != 0 && _a_close == true){
+			::close(_fd);
+		}
+	}
+
+
+	int RawOutputStream::fd(){
+		return _fd;
+
+	}
+
+	RawOutputStream &RawOutputStream::write(void *data, UInt32 size){
+		if(::write(_fd, data, size) == -1){
+			throw StreamException(errno);
+		}
+		return *this;
+	}
+
+	void RawOutputStream::flush(){
+		if(fsync(fd()) == -1){
+			throw StreamException(errno);
+		}
+	}
+
+	FileSizeT RawOutputStream::size(){
+		flush();
+		return RawStreamInterface::size();
+	}
+
+	bool RawOutputStream::eof(){
+		return true;
+	}
+
+	bool RawOutputStream::ready(){
+		int r;
+		bool ret = false;
+		struct pollfd d = { _fd, POLLOUT, 0 };
+		r = poll(&d, 1, 0);
+		if(r == -1){
+			throw StreamException(errno);
+		}
+		else if(r > 0){
+			if(d.revents & POLLOUT == POLLOUT){
+				ret = true;
+			}
+		}
+		return ret;
+	}
+};
+
---
+++ trunk/X-VR2/libs/xvr2core/xvr2/RawOutputStream.h
@@ -0,0 +1,44 @@
+/* $Id$ */
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#ifndef __XVR2_RAW_OUTPUT_STREAM_H__
+#define __XVR2_RAW_OUTPUT_STREAM_H__
+#include<xvr2/StreamInterface.h>
+#include<xvr2/RawStreamInterface.h>
+#include<xvr2/SystemException.h>
+
+namespace xvr2 {
+	
+	class RawOutputStream : public WriteableStream, public RawStreamInterface {
+		public:
+			RawOutputStream();
+			virtual ~RawOutputStream();
+			RawOutputStream(int __fd);
+			RawOutputStream(const String &fname);
+			void open(int __fd);
+			void open(const String &fname);
+			virtual void close();
+
+			int fd();
+			virtual RawOutputStream &write(void *data, UInt32 size);
+			virtual void flush();
+			FileSizeT size();
+			bool eof();
+			virtual bool ready();
+	};
+};
+#endif
+
