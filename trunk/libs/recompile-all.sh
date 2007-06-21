5a98f166536a52c086fd0e0ce09f23cc27804428
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2007-06-21 16:04:34 +0000

    Preparing to deprecate the ConfigFile class from the Util namespace
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@515 07303567-df1b-0410-b539-9fd000affbe4

--- trunk/libs/recompile-all.sh
+++ trunk/libs/recompile-all.sh
@@ -7,6 +7,12 @@ export CXXFLAGS=-gstabs+
 
 LOGFILE=/tmp/_xvr2_autogen_all.tmp.log
 
+uname -a | grep -i sunos > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	export PATH=/usr/ccs/bin:/usr/sfw/bin:/usr/local/bin:$PATH
+	export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/local/lib/pkgconfig
+fi
+
 for library in `cat COMPILATION_ORDER | grep -v '^\#'`
 do
 	cd $library
@@ -34,19 +40,24 @@ do
 	if [ $? -ne 0 ]; then
 		exit 1
 	fi
-	echo -ne "\n\nDo you wish to install this library ($library) [Y/n] "
-	#read yesno
-	if [ "$yesno" != 'n' ]; then
-		cmd=`which sudo`
-		if [ -x ${cmd} ]; then
-			sudo make install
-			if [ $? -ne 0 ]; then
-				exit 1
-			fi
-		else
-			su - root -c make install
-			if [ $? -ne 0 ]; then
-				exit 1
+	$u=`whoami`
+	if [ "$u" = root ]; then
+		make install
+	else
+		echo -ne "\n\nDo you wish to install this library ($library) [Y/n] "
+		#read yesno
+		if [ "$yesno" != 'n' ]; then
+			cmd=`which sudo`
+			if [ -x ${cmd} ]; then
+				sudo make install
+				if [ $? -ne 0 ]; then
+					exit 1
+				fi
+			else
+				su - root -c make install
+				if [ $? -ne 0 ]; then
+					exit 1
+				fi
 			fi
 		fi
 	fi
