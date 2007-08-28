04632dc4e4f167e2ffeb61e35a4be00e5f5fedb3
mindstorm2600 <mindstorm2600@07303567-df1b-0410-b539-9fd000affbe4>
2007-08-28 21:03:04 +0000

    Adding a new xvr2::Vector test program.
    git-svn-id: https://xvr2.svn.sourceforge.net/svnroot/xvr2@547 07303567-df1b-0410-b539-9fd000affbe4

---
+++ trunk/Core/examples/vector-test.cpp
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ *
+ * X-VR2 
+ * 
+ * Copyright (C) Juan V. Guerrero 2007
+ * 
+ * Juan V. Guerrero <mindstorm2600@users.sourceforge.net>
+ * 
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+#include<xvr2/Vector.h>
+#include<iostream>
+
+template<typename _Tp>
+void print_r(const std::vector<_Tp> &arr){
+	std::cout << "[";
+	for(_Tp i = 0; i < (_Tp)arr.size(); i++){
+		if(i > 0){
+			std::cout << ",";
+		}
+		std::cout << arr[i];
+	}
+	std::cout << "]" << std::endl;
+}
+
+int main(int argc, char *argv[]){
+	
+	xvr2::Vector<int> a;
+	a.push_back(0);
+	a.push_back(1);
+	a.push_back(2);
+	a.push_back(3);
+	std::vector<int> b;
+	
+	b = a;
+	std::cout << "a: " << a.toString() << std::endl;
+	std::cout << "b: ";
+	print_r<int>(b);
+	return 0;
+}
